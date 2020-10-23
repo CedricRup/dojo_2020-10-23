701598bb189bf4dd099b16b0a94e5496f712d576
Souhail SOUID <mr.souid@live.fr>
2020-10-23 15:10:08 +0200

    dojo

--- theObservedPIN.js
+++ theObservedPIN.js
@@ -1,5 +1,17 @@
- function getPINs (observed)  {
-    return [];
+function getPINs(observed) {
+    const variationsOf = {
+        "0": ["0", "8"],
+        "1": ["1", "2", "4"],
+        "5": ["2", "4", "5", "6", "8"]
+
+    }
+    if (observed.length === 0) {
+       return [];
+    }
+    return variationsOf[observed] 
+    
+    
+
 }
 
 exports.getPINs = getPINs
--- theObservedPIN.test.js
+++ theObservedPIN.test.js
@@ -1,4 +1,13 @@
 const observedPin  = require("./theObservedPIN")
-it ("un pin vide donne un résultat vide",()=>{
+it ("un pin vide donne un résultat vide",()=> {
     expect(observedPin.getPINs("")).toStrictEqual([]);
+})
+it ("un pin qui est égal à 0  donne un résultat 0 ou 8",() => {
+    expect(observedPin.getPINs("0")).toStrictEqual(["0", "8"]);
+})
+it ("un pin qui est égal à 1  donne un résultat 1 ou 2 ou 4",() => {
+    expect(observedPin.getPINs("1")).toStrictEqual(["1", "2", "4"]);
+})
+it ("un pin qui est égal à 5  donne un résultat   2 ou 4 ou 5 ou 6 ou 8",() => {
+    expect(observedPin.getPINs("5")).toStrictEqual(["2", "4", "5", "6", "8"]);
 })
