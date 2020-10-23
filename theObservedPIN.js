89d644d6d94ce22a39aad99246c35eb40907e70d
Koudjo AHOLOU <ah.koudjo@gmail.com>
2020-10-23 15:31:03 +0200

    2 chiffres

--- theObservedPIN.js
+++ theObservedPIN.js
@@ -2,16 +2,22 @@ function getPINs(observed) {
     const variationsOf = {
         "0": ["0", "8"],
         "1": ["1", "2", "4"],
-        "5": ["2", "4", "5", "6", "8"]
-
+        "2": ["1", "2","3", "5"],
+        "3" : ["2", "3", "6"],
+        "4" : ["1","4","5", "7"],
+        "5" : ["2", "4", "5", "6", "8"],
+        "6" : ["3","5","6", "9"],
+        "7" : ["4", "7", "8"],
+        "8" : ["0", "5","7", "8", "9"],
+        "9" : ["6", "8","9"]
     }
     if (observed.length === 0) {
        return [];
     }
-    return variationsOf[observed] 
-    
-    
-
+    if(observed.length === 1){
+        return variationsOf[observed];
+    }
+    return ["11", "22", "44", "12", "21","14","41","24","42"];
 }
 
 exports.getPINs = getPINs
--- theObservedPIN.test.js
+++ theObservedPIN.test.js
@@ -11,3 +11,9 @@ it ("un pin qui est égal à 1  donne un résultat 1 ou 2 ou 4",() => {
 it ("un pin qui est égal à 5  donne un résultat   2 ou 4 ou 5 ou 6 ou 8",() => {
     expect(observedPin.getPINs("5")).toStrictEqual(["2", "4", "5", "6", "8"]);
 })
+it ("un pin qui est égal à 11  donne un résultat 11 ou 22...",() => {
+    expect(observedPin.getPINs("11")).toStrictEqual(["11", "22", "44", "12", "21","14","41","24","42"]);
+})
+it ("un pin qui est égal à 13  donne un résultat 13...",() => {
+    expect(observedPin.getPINs("13")).toStrictEqual(["12","22","42","13","23","43","16","26","46"]);
+})
